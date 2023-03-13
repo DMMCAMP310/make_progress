9a933d366f6d0d6e3b0821725e7e21002d9a26d3
DMMCAMP310 <kuon6173@gmail.com>
2023-03-13 01:26:32 +0000

    ユーザー関連の作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def edit
+  end
+  
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :section, :years, :profile_image)
+  end
+end
