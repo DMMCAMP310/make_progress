da73aba256580f1bc37bda2be6f70826c6b7b846
DMMCAMP310 <kuon6173@gmail.com>
2023-03-24 13:11:58 +0000

    カテゴリー絞り込み

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,13 +4,6 @@ class Category < ApplicationRecord
   def self.major_categories
     pluck(:major_category_name).uniq
   end
-  scope :request_category, -> (category) {
-    if category != "none"
-      find(category.to_i)
-    else
-      ""
-    end
-  }
   
   scope :request_category, -> (category) {
     if category != "none"
